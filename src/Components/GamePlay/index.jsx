bd34fb8299c3693417ea65f931acd7d828ce2178
Alexey <stierlitzotto21@gmail.com>
2022-06-29 14:51:25 +0900

    Game v-0.3

--- src/Components/GamePlay/index.jsx
+++ src/Components/GamePlay/index.jsx
@@ -1,15 +1,17 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ContextState } from "../../Context";
 import "./index.scss";
 
 export const GamePlay = () => {
   const context = useContext(ContextState);
+  const [massiveCoincidences, setMassiveCoincidences] = useState();
   const lengthRandNumber = context.randNumber.toString().split("");
 
   function fractionMassives() {
     let fractionNumber = context.randNumber.toString().split("");
     let fractionValue = context.value.toString().split("");
     checkMassive(fractionNumber, fractionValue);
+    checkMassiveSymbols(fractionNumber, fractionValue);
   }
 
   const checkMassive = (fractionNumber, fractionValue) => {
@@ -29,6 +31,33 @@ export const GamePlay = () => {
     }
   };
 
+  const checkMassiveSymbols = (fractionNumber, fractionValue) => {
+    let massiveFirst = [];
+    let massiveSecond = [];
+    let massiveFindNumber = [];
+    const stringFirst = [...fractionNumber];
+    const stringSecond = [...fractionValue];
+ 
+    // Убираем совпавшие цифры на своих местах.
+    for (let i = 0; i < fractionNumber.length; i++) {
+      if (stringFirst[i] !== stringSecond[i]) {
+        massiveFirst.push(stringFirst[i]);
+        massiveSecond.push(stringSecond[i]);
+      }
+    }
+
+    // Совпавших цифр не на своих местах.
+    for (let i = 0; i < massiveFirst.length; i++) {
+      massiveFirst.filter((e) => {
+        if (e == massiveSecond[i]) {
+          massiveFindNumber.push("X")
+        }
+      });
+    }
+
+    setMassiveCoincidences(massiveFindNumber);
+  };
+
   return (
     <div className="container">
       <input
@@ -40,19 +69,21 @@ export const GamePlay = () => {
         maxLength={lengthRandNumber.length}
         placeholder="Ваше число?"
       />
-
       <button
         disabled={context.randNumber == 0}
         className="button"
         onClick={() => fractionMassives()}>
         Крути барабан
       </button>
-
-      {context.attempt != 0 ? (
+      {context.attempt ? (
         <div className="dialog">
           <div className="dialog__text">
             Совпавших цифр на своих местах:
             <div className="dialog__number">{context.attempt.length}</div>
+          </div>
+          <div className="dialog__text">
+            Совпавших цифр не на своих местах:
+            <div className="dialog__number">{massiveCoincidences.length}</div>
           </div>
         </div>
       ) : (
